211b5da4cc15b82f06bd74f022367b7c3fa68580
dmitrdix <dmitr_dix@mail.ru>
2024-09-09 23:26:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+print ((9**0.5)*5) # "1st programm"
+print (9.99>9.98 and 1000!=1000.1)  # "2st programm"
+print (2*2+2) # "3st programm"
+print (2*(2+2))
+print ((2*2+2)==(2*(2+2)))
+print (float('123.456')) # "4st programm"
+print (123.4568*10)
+print (int(1234.568))
+print  (1234%10)
